003ecf5dd85dffdc3f79e2eff022b266ffd01e5d
iskin20003@gmail.com <iskin20003@gmail.com>
2021-04-26 00:03:20 +0300

    Add requirements.txt file

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from data.baskets import Basket
 from forms.user_form import *
 from forms.product_form import ProductForm
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -157,5 +158,13 @@ def addproduct():
     return render_template('addproduct.html', form=form)
 
 
+# if __name__ == '__main__':
+#     main()
+
+
 if __name__ == '__main__':
-    main()
+    db_session.global_init("db/blogs.db")
+    db_sess = db_session.create_session()
+    db_sess.commit()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
